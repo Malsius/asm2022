ea82ecda6c3672b18206bd3a0cad7c432e07a258
Malsius <admin@malsius.com>
2022-02-14 00:20:30 +0100

    add fibonnaci

--- fibonnaci/fibonnaci.c
+++ fibonnaci/fibonnaci.c
@@ -6,6 +6,8 @@ long long fibonnaci(long long);
 int main() {
 
     printf("fibonnaci(8)=%lli\n", fibonnaci(8)); // 21
+    printf("fibonnaci(0)=%lli\n", fibonnaci(0)); // 0
+    printf("fibonnaci(16)=%lli\n", fibonnaci(16)); // 987
     printf("fibonnaci(-5)=%lli\n", fibonnaci(-5)); // -1
     return 0;
 }
