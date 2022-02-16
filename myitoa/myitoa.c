361b474498bc9267a0c7ee69b7a21686dc53f209
Malsius <admin@malsius.com>
2022-02-16 14:04:29 +0100

    add myitoa

--- myitoa/myitoa.c
+++ myitoa/myitoa.c
@@ -7,7 +7,7 @@ int main() {
     printf("myitoa(\"5\")=%lli\n", myitoa("5")); // 5
     printf("myitoa(\"-5\")=%lli\n", myitoa("-5")); // -5
     printf("myitoa(\"-18\")=%lli\n", myitoa("-18")); // -18
-
+    printf("myitoa(\"-1234567\")=%lli\n", myitoa("-1234567")); // -1234567
 }
 
 
