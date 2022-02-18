d5f0bbb02d950d8a76e6de2de21ba59ac8650d5d
Malsius <admin@malsius.com>
2022-02-18 21:11:48 +0100

    add myorderlist

--- myorderlist/myorderlist.c
+++ myorderlist/myorderlist.c
@@ -2,11 +2,11 @@
 
 void orderlist(long long *list, int size);
 
-long long li[] = {0x10, 0x3, 0x8};
+long long li[] = {0x10, 0x0, 0x8, 0x10, 0xFFFFFFFF};
 
 int main() {
     
-    orderlist(li, 3);
+    orderlist(li, 5);
     // print: "3, 8, 16\n"
 
     return 0;
